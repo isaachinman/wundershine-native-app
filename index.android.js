7333981c2e3540ef9d19815fcfaa1ab60884b555
Isaac Hinman <isaac@isaachinman.com>
2018-04-06 12:23:11 +0200

    Enable layout animations on Android

--- index.android.js
+++ index.android.js
@@ -1,7 +1,12 @@
-import { AppRegistry } from 'react-native'
+import { AppRegistry, UIManager } from 'react-native'
 
 import App from './src'
 import Share from './src/components/ShareReceiver/ShareReceiver'
+
+// Enable layout animations
+if (UIManager.setLayoutAnimationEnabledExperimental) {
+  UIManager.setLayoutAnimationEnabledExperimental(true)
+}
 
 AppRegistry.registerComponent('Wundershine', () => App)
 AppRegistry.registerComponent('ShareReceiver', () => Share)
