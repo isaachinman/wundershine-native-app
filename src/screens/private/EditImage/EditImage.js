bc82d80d12c0a6c5f38eaa3dfff9816bff145232
Isaac Hinman <isaac@isaachinman.com>
2018-04-15 14:15:57 +0200

    Starting zoom functionality, lots of functionality missing

--- src/screens/private/EditImage/EditImage.js
+++ src/screens/private/EditImage/EditImage.js
@@ -5,7 +5,10 @@ import { inject, observer } from 'mobx-react'
 import { NavActions, screenUtils } from 'utils/nav'
 
 import { Button } from 'components'
-import { Image, PanResponder, View } from 'react-native'
+import { Image, View } from 'react-native'
+import { createResponder } from 'react-native-gesture-responder'
+
+import { SQUARE, PORTRAIT, LANDSCAPE } from 'utils/images/aspect-ratios'
 
 const SQUARE_FRAME_DIMENSION = 300
 
@@ -30,25 +33,31 @@ export default class EditImage extends React.Component {
 
   componentWillMount = () => {
 
+    // Find master image from queue
     const { _id, queue } = this.props
-
     this.masterImage = queue.data.images.find(i => i._id === _id)
 
-    this.applyTransformation()
-
-    this.imageStyles = {
-      width: this.adjustedWidth,
-      height: this.adjustedHeight,
-      top: this.topOffset,
-      left: this.leftOffset,
+    // Determine aspect ratio and layout
+    this.aspectRatio = this.masterImage.width / this.masterImage.height
+    if (this.aspectRatio < 1) {
+      this.layout = PORTRAIT
+    } else if (this.aspectRatio === 1) {
+      this.layout = SQUARE
+    } else if (this.aspectRatio > 1) {
+      this.layout = LANDSCAPE
     }
 
-    this.panResponder = PanResponder.create({
-      onStartShouldSetPanResponder: () => true,
-      onMoveShouldSetPanResponder: () => true,
-      onPanResponderMove: this.handlePanResponderMove,
-      onPanResponderRelease: this.handlePanResponderEnd,
-      onPanResponderTerminate: this.handlePanResponderEnd,
+    // Apply initial transformation
+    this.applyTransformation()
+
+    this.gestureResponder = createResponder({
+      onStartShouldSetResponder: () => true,
+      onStartShouldSetResponderCapture: () => true,
+      onMoveShouldSetResponder: () => true,
+      onMoveShouldSetResponderCapture: () => true,
+      onResponderMove: this.handleResponderMove,
+      onResponderRelease: this.handleResponderEnd,
+      onResponderTerminate: this.handleResponderEnd,
     })
 
   }
@@ -84,6 +93,13 @@ export default class EditImage extends React.Component {
 
     this.rightLimit = this.leftOffset - ((adjustedWidth + this.leftOffset) - SQUARE_FRAME_DIMENSION)
     this.bottomLimit = this.topOffset - ((adjustedHeight + this.topOffset) - SQUARE_FRAME_DIMENSION)
+
+    this.imageStyles = {
+      width: this.adjustedWidth,
+      height: this.adjustedHeight,
+      top: this.topOffset,
+      left: this.leftOffset,
+    }
 
   }
 
@@ -134,6 +150,11 @@ export default class EditImage extends React.Component {
     if (leftVal <= this.rightLimit) {
       leftVal = this.rightLimit
     }
+
+    if (this.verticalLetterboxMargin) {
+      leftVal = this.verticalLetterboxMargin
+    }
+
     return leftVal
   }
 
@@ -147,17 +168,69 @@ export default class EditImage extends React.Component {
     if (topVal <= this.bottomLimit) {
       topVal = this.bottomLimit
     }
+
+    if (this.verticalLetterboxMargin) {
+      if (this.imageStyles.height <= SQUARE_FRAME_DIMENSION) {
+        topVal = 0
+      }
+      if (topVal >= 0) {
+        topVal = 0
+      }
+    }
+
     return topVal
   }
 
-  handlePanResponderMove = (e, gestureState) => {
-    this.xShift = this.calculateXAxis(this.previousLeft, gestureState.dx)
-    this.yShift = this.calculateYAxis(this.previousTop, gestureState.dy)
-    this.imageStyles.top = this.calculateYAxis(this.previousTop, gestureState.dy)
+  calculateZoom = (gestureState) => {
+    const zoomFactor = gestureState.pinch - gestureState.previousPinch
+
+    const ratio = (this.imageStyles.width + zoomFactor) / this.imageStyles.width
+
+    const oldWidth = this.imageStyles.width
+    const oldHeight = this.imageStyles.height
+
+    const newWidth = this.imageStyles.width + zoomFactor
+    const newHeight = this.imageStyles.height * ratio
+
+    let xShift = this.xShift - ((newWidth - oldWidth) / 2)
+    let yShift = this.yShift - ((newHeight - oldHeight) / 2)
+
+    this.verticalLetterboxMargin = null
+    this.horizontalLetterboxMargin = null
+
+    // Portrait limit
+    if (newWidth < SQUARE_FRAME_DIMENSION) {
+      this.verticalLetterboxMargin = (SQUARE_FRAME_DIMENSION - newWidth) / 2
+      xShift = this.verticalLetterboxMargin
+      if (newHeight <= SQUARE_FRAME_DIMENSION) {
+        return false
+      }
+      if (yShift >= 0) {
+        yShift = 0
+      }
+    }
+
+    this.imageStyles.width = newWidth
+    this.imageStyles.height = newHeight
+
+    this.xShift = xShift
+    this.yShift = yShift
+
+    return true
+
+  }
+
+  handleResponderMove = (e, gestureState) => {
+    if (gestureState.pinch) {
+      this.calculateZoom(gestureState)
+    } else {
+      this.xShift = this.calculateXAxis(this.previousLeft, gestureState.dx)
+      this.yShift = this.calculateYAxis(this.previousTop, gestureState.dy)
+    }
     this.updateNativeStyles()
   }
 
-  handlePanResponderEnd = (e, gestureState) => {
+  handleResponderEnd = (e, gestureState) => {
     this.previousLeft = this.calculateXAxis(this.previousLeft, gestureState.dx)
     this.previousTop = this.calculateYAxis(this.previousTop, gestureState.dy)
   }
@@ -168,7 +241,7 @@ export default class EditImage extends React.Component {
       <View>
         <View style={styles.frame}>
           <Image
-            {...this.panResponder.panHandlers}
+            {...this.gestureResponder}
             ref={i => this.image = i}
             source={{ uri: this.masterImage.uri }}
             style={this.imageStyles}
