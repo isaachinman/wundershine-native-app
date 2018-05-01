e105ea9a6e96f29db096d936dcba5920d6cf8e54
Isaac Hinman <isaac@isaachinman.com>
2018-05-01 11:39:16 +0200

    Add app groups for iOS and react-native-fs to fix iOS share extension issue

--- android/app/src/main/java/com/wundershine/app/MainApplication.java
+++ android/app/src/main/java/com/wundershine/app/MainApplication.java
@@ -14,6 +14,7 @@ import com.wix.interactable.Interactable;
 
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactApplication;
+import com.rnfs.RNFSPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.facebook.react.ReactNativeHost;
@@ -57,7 +58,8 @@ public class MainApplication extends NavigationApplication {
           new UploaderReactPackage(),
           new RNFetchBlobPackage(),
           new Interactable(),
-          new PickerPackage()
+          new PickerPackage(),
+          new RNFSPackage()
         );
     }
 
--- src/components/ShareReceiver/ShareReceiver.js
+++ src/components/ShareReceiver/ShareReceiver.js
@@ -1,17 +1,41 @@
 import React from 'react'
 
 import { Linking, Platform } from 'react-native'
+import path from 'react-native-path'
+import RNFS from 'react-native-fs'
 import ShareExtension from 'utils/share-extension'
 
 export default class Share extends React.Component {
 
   async componentDidMount() {
     try {
+
       const data = await ShareExtension.data()
-      const urlSafeData = JSON.stringify({ data })
-      const url = `wundershine://ImageQueue/ShareReceive?shareReceivingImages=${urlSafeData}`
+      let urlSafeData = JSON.stringify({ data })
+      let url = `wundershine://ImageQueue/ShareReceive?shareReceivingImages=${urlSafeData}`
 
       if (Platform.OS === 'ios') {
+        const appGroupPath = await RNFS.pathForGroup('group.com.wundershine')
+        const transformedData = []
+
+        await Promise.all(data.map(async (image) => {
+          const filename = path.basename(image.value)
+          const destination = `${appGroupPath}/${filename}`
+          const fileAlreadyExists = await RNFS.exists(destination)
+
+          if (!fileAlreadyExists) {
+            await RNFS.copyFile(image.value, destination)
+          }
+
+          transformedData.push({
+            type: image.type,
+            value: destination,
+          })
+        }))
+
+        urlSafeData = JSON.stringify({ data: transformedData })
+        url = `wundershine://ImageQueue/ShareReceive?shareReceivingImages=${urlSafeData}`
+
         await ShareExtension.openURL(url)
       } else {
         await Linking.openURL(url)
