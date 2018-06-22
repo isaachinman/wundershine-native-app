b68b76a7c3585c962b55b79f76f6fba9d58f733f
Isaac Hinman <isaac@isaachinman.com>
2018-06-22 13:01:44 +0200

    Set max width for SQUARE_FRAME_DIMENSION

--- src/screens/private/EditImage/constants.js
+++ src/screens/private/EditImage/constants.js
@@ -1,6 +1,12 @@
 import { Dimensions } from 'react-native'
 
 export const SQUARE_FRAME_MARGIN = 30
-export const SQUARE_FRAME_DIMENSION = (Dimensions.get('window').width - (SQUARE_FRAME_MARGIN * 2)) * 0.91304347828
+
+let squareFrameDimension = (Dimensions.get('window').width - (SQUARE_FRAME_MARGIN * 2)) * 0.91304347828
+if (squareFrameDimension > (Dimensions.get('window').height * 0.42)) {
+  squareFrameDimension = Dimensions.get('window').height * 0.42
+}
+
+export const SQUARE_FRAME_DIMENSION = squareFrameDimension
 export const SQUARE_FRAME_PADDING = SQUARE_FRAME_DIMENSION * 0.04347826086
 export const SQUARE_FRAME_CHIN = SQUARE_FRAME_DIMENSION * 0.39130434782
