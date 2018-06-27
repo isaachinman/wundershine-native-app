17412536ed7427b64c5bdf0aaa72101fb6955f50
Isaac Hinman <isaac@isaachinman.com>
2018-06-27 17:01:13 +0200

    Swap frame image, increase size on screen

--- src/screens/private/PackReview/constants.js
+++ src/screens/private/PackReview/constants.js
@@ -1,12 +1,12 @@
 /* eslint-disable max-len */
 import { Dimensions } from 'react-native'
 
-export const SQUARE_REVIEW_FRAME_MARGIN = 50
+export const SQUARE_REVIEW_FRAME_MARGIN = 35
 export const SQUARE_REVIEW_FRAME_DIMENSION = (Dimensions.get('window').width - (SQUARE_REVIEW_FRAME_MARGIN * 2))
 
 // This number is the distance from the edge of image to the edge
 // of print (times two), divided by the total width of the image
 export const SQUARE_REVIEW_PRINT_MARGIN = (SQUARE_REVIEW_FRAME_MARGIN) +
-  (((SQUARE_REVIEW_FRAME_DIMENSION) * 0.46191051995) / 2)
+  (((SQUARE_REVIEW_FRAME_DIMENSION) * 0.49666666666) / 2)
 export const SQUARE_REVIEW_PRINT_DIMENSION =
-  (SQUARE_REVIEW_FRAME_DIMENSION) * 0.53808948005
+  (SQUARE_REVIEW_FRAME_DIMENSION) * 0.50333333334
