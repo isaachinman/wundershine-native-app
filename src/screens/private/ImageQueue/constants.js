57c703a182cc8baf756b196161eb514de8c03763
Isaac Hinman <isaac@isaachinman.com>
2018-06-27 12:39:16 +0200

    Increase QUEUE_PADDING_BOTTOM

--- src/screens/private/ImageQueue/constants.js
+++ src/screens/private/ImageQueue/constants.js
@@ -1,4 +1,4 @@
 export const QUEUE_ITEM_HEIGHT = 130
 export const QUEUE_IMAGE_DIMENSION = 100
 export const QUEUE_ICON_SIZE = 34
-export const QUEUE_PADDING_BOTTOM = 140
+export const QUEUE_PADDING_BOTTOM = 300
