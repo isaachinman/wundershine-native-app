871ea6b93d81d5dc307086a55c14907c68728660
Isaac Hinman <isaac@isaachinman.com>
2018-07-03 12:53:04 +0200

    Don't remove localImageOverlay at all

--- src/screens/private/ImageQueue/subcomponents/QueueItem.js
+++ src/screens/private/ImageQueue/subcomponents/QueueItem.js
@@ -166,7 +166,6 @@ export default class QueueItem extends React.Component {
   onLoad = () => {
     this.setState({
       thumbnailLoading: false,
-      localImageOverlay: this.props.uriIsLocal,
     })
   }
 
