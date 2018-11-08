f5850c2d11e86c6f9318a584ff79ff5f189dbe7b
Isaac Hinman <isaac@isaachinman.com>
2018-11-07 18:11:31 -0800

    Move CLOUDINARY_CLOUD_NAME to env

--- src/config.js
+++ src/config.js
@@ -10,18 +10,12 @@ const appConfig = {
   ...NativeModules.RNUeno,
   __native: config,
   __js: env,
-  cloudinaryCloudName: 'wundershine-staging',
 }
 
-// Production settings
-if (env.BUILD_TRACK === 'production') {
-  appConfig.cloudinaryCloudName = 'wundershine'
-}
-
-// Dev settings
-if (process.env.NODE_ENV === 'development') {
-  appConfig.API_ROOT = 'http://localhost:8080/v1'
-  appConfig.STRIPE_PUBLISHABLE_KEY = 'pk_test_OpL5YE34O9ByYmg6zuLARaFs'
-}
+// // Dev settings
+// if (process.env.NODE_ENV === 'development') {
+//   appConfig.API_ROOT = 'http://localhost:8080/v1'
+//   appConfig.STRIPE_PUBLISHABLE_KEY = 'pk_test_OpL5YE34O9ByYmg6zuLARaFs'
+// }
 
 export default appConfig
--- src/utils/images/cloudinary.js
+++ src/utils/images/cloudinary.js
@@ -2,6 +2,6 @@ import cloudinaryCore from 'cloudinary-core'
 import config from 'config'
 
 export default new cloudinaryCore.Cloudinary({
-  cloud_name: config.cloudinaryCloudName,
+  cloud_name: config.CLOUDINARY_CLOUD_NAME,
   secure: true,
 })
